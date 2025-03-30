6ac57c508bdc4614e197d0d159f1d158c961e93d
Mark Severin <marksew@yandex.ru>
2025-03-30 14:06:28 +0300

    Update Yandex Maps type declarations to use ambient declarations

--- src/types/yandex-maps.d.ts
+++ src/types/yandex-maps.d.ts
@@ -1,47 +1,54 @@
-declare module 'yandex-maps' {
-  export interface YMaps {
-    Map: any;
-    Placemark: any;
-    GeoObject: any;
-    MapType: any;
-    ZoomControl: any;
-    TypeControl: any;
-    ScaleLine: any;
-    SearchControl: any;
-    TrafficControl: any;
-    FullscreenControl: any;
-    GeolocationControl: any;
-    RouteEditor: any;
-    RouteButton: any;
-    RoutePanel: any;
-    Route: any;
-    Polyline: any;
-    Rectangle: any;
-    Circle: any;
-    Polygon: any;
-    GeoQueryResult: any;
-    GeoQuery: any;
-    GeoQueryResultSet: any;
-    GeoQueryResultCollection: any;
-    GeoQueryResultFeature: any;
-    GeoQueryResultFeatureCollection: any;
-    GeoQueryResultGeometry: any;
-    GeoQueryResultGeometryCollection: any;
-    GeoQueryResultPoint: any;
-    GeoQueryResultMultiPoint: any;
-    GeoQueryResultLineString: any;
-    GeoQueryResultMultiLineString: any;
-    GeoQueryResultPolygon: any;
-    GeoQueryResultMultiPolygon: any;
-  }
+interface YMaps {
+  Map: any;
+  Placemark: any;
+  GeoObject: any;
+  MapType: any;
+  ZoomControl: any;
+  TypeControl: any;
+  ScaleLine: any;
+  SearchControl: any;
+  TrafficControl: any;
+  FullscreenControl: any;
+  GeolocationControl: any;
+  RouteEditor: any;
+  RouteButton: any;
+  RoutePanel: any;
+  Route: any;
+  Polyline: any;
+  Rectangle: any;
+  Circle: any;
+  Polygon: any;
+  GeoQueryResult: any;
+  GeoQuery: any;
+  GeoQueryResultSet: any;
+  GeoQueryResultCollection: any;
+  GeoQueryResultFeature: any;
+  GeoQueryResultFeatureCollection: any;
+  GeoQueryResultGeometry: any;
+  GeoQueryResultGeometryCollection: any;
+  GeoQueryResultPoint: any;
+  GeoQueryResultMultiPoint: any;
+  GeoQueryResultLineString: any;
+  GeoQueryResultMultiLineString: any;
+  GeoQueryResultPolygon: any;
+  GeoQueryResultMultiPolygon: any;
+}
 
-  export interface YMapsInstance {
-    ready: () => Promise<YMaps>;
-  }
+interface YMapsInstance {
+  ready: () => Promise<YMaps>;
+}
 
-  export interface YMapsScript {
-    load: () => Promise<YMapsInstance>;
-  }
+interface YMapsScript {
+  load: () => Promise<YMapsInstance>;
+}
 
-  export function load(apiKey: string): Promise<YMapsInstance>;
-} 
+declare global {
+  interface Window {
+    ymaps: {
+      ready: () => Promise<YMaps>;
+      load: (apiKey: string) => Promise<YMapsInstance>;
+    };
+  }
+}
+
+export {}; 
